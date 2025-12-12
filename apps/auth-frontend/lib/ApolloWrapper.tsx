b2968dc0a9617b8d4f64d22d4d2e8f05be16377d
Oscar Cortez <oscarkortez@gmail.com>
2025-12-11 22:37:24 -0400

    fixed rehidration

--- apps/auth-frontend/lib/ApolloWrapper.tsx
+++ apps/auth-frontend/lib/ApolloWrapper.tsx
@@ -3,24 +3,8 @@
 import { PropsWithChildren } from 'react';
 import { HttpLink, ApolloLink } from '@apollo/client';
 import { ApolloNextAppProvider, ApolloClient, InMemoryCache } from '@apollo/client-integration-nextjs';
-import Cookies from 'js-cookie';
 
 const client = () => {
-  // Auth middleware que agrega el token al header
-  const authLink = new ApolloLink((operation, forward) => {
-    // Obtén el token del localStorage
-    const token = typeof window !== 'undefined' ? Cookies.get('auth_token') : null;
-
-    // Agrega el header Authorization con Bearer token
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : '',
-      },
-    });
-
-    return forward(operation);
-  });
-
   const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
     fetchOptions: { cache: 'no-store' },
@@ -28,7 +12,7 @@ const client = () => {
 
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: ApolloLink.from([authLink, httpLink]), // ✅ authLink primero, luego httpLink
+    link: ApolloLink.from([httpLink]), // ✅ authLink primero, luego httpLink, [authLink],
     devtools: {
       enabled: process.env.NODE_ENV !== 'production', // Habilita devtools solo en desarrollo
     },
